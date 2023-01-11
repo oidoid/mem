e9f0ac636456917055723f4f29f48f23846bc336
Stephen Niedzielski <stephen@niedzielski.com>
2023-01-11 00:03:54 -0700

    shish-kebab-case filenames

--- mod.ts
+++ mod.ts
@@ -1,4 +1,4 @@
-import type { FontMeta } from './src/FontMeta.ts';
+import type { FontMeta } from './src/font-meta.ts';
 import mono3x3 from './src/mem-mono-3x3.json' assert { type: 'json' };
 import mono4x4 from './src/mem-mono-4x4.json' assert { type: 'json' };
 import prop3x5 from './src/mem-prop-3x5.json' assert { type: 'json' };
@@ -6,7 +6,7 @@ import prop4x4 from './src/mem-prop-4x4.json' assert { type: 'json' };
 import prop5x5 from './src/mem-prop-5x5.json' assert { type: 'json' };
 import prop5x6 from './src/mem-prop-5x6.json' assert { type: 'json' };
 
-export * from './src/FontMeta.ts';
+export * from './src/font-meta.ts';
 export const memMono3x3: FontMeta = mono3x3;
 export const memMono4x4: FontMeta = mono4x4;
 export const memProp3x5: FontMeta = prop3x5;
