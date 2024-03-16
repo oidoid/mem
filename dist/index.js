6c577383d34ddf641a9b9d751d87397c65ae285a
Stephen Niedzielski <stephen@oidoid.com>
2024-03-16 15:12:33 -0600

    Fix compilation inputs

---
+++ dist/index.js
@@ -0,0 +1,13 @@
+import mono3x3 from './mem-mono-3x3.json' with { type: 'json' };
+import mono4x4 from './mem-mono-4x4.json' with { type: 'json' };
+import prop3x5 from './mem-prop-3x5.json' with { type: 'json' };
+import prop4x4 from './mem-prop-4x4.json' with { type: 'json' };
+import prop5x5 from './mem-prop-5x5.json' with { type: 'json' };
+import prop5x6 from './mem-prop-5x6.json' with { type: 'json' };
+export const memMono3x3 = mono3x3;
+export const memMono4x4 = mono4x4;
+export const memProp3x5 = prop3x5;
+export const memProp4x4 = prop4x4;
+export const memProp5x5 = prop5x5;
+export const memProp5x6 = prop5x6;
+//# sourceMappingURL=index.js.map
