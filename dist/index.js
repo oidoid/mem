3906bb08ac77b552153744368446af94932f3931
Stephen Niedzielski <stephen@oidoid.com>
2024-03-22 21:22:53 -0600

    [release] 11.0.0-2

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,9 @@
-import mono3x3 from './mem-mono-3x3.json' with { type: 'json' };
-import mono4x4 from './mem-mono-4x4.json' with { type: 'json' };
-import prop3x5 from './mem-prop-3x5.json' with { type: 'json' };
-import prop4x4 from './mem-prop-4x4.json' with { type: 'json' };
-import prop5x5 from './mem-prop-5x5.json' with { type: 'json' };
-import prop5x6 from './mem-prop-5x6.json' with { type: 'json' };
+import mono3x3 from './mem-mono-3x3.json' assert { type: 'json' };
+import mono4x4 from './mem-mono-4x4.json' assert { type: 'json' };
+import prop3x5 from './mem-prop-3x5.json' assert { type: 'json' };
+import prop4x4 from './mem-prop-4x4.json' assert { type: 'json' };
+import prop5x5 from './mem-prop-5x5.json' assert { type: 'json' };
+import prop5x6 from './mem-prop-5x6.json' assert { type: 'json' };
 export const memMono3x3 = mono3x3;
 export const memMono4x4 = mono4x4;
 export const memProp3x5 = prop3x5;
